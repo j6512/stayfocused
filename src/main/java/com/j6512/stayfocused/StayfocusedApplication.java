e470c35072e797b5f096a87e618b7df94872c033
j6512 <justins8287@gmail.com>
2021-04-16 16:23:47 -0500

    Beginning of project

---
+++ src/main/java/com/j6512/stayfocused/StayfocusedApplication.java
@@ -0,0 +1,13 @@
+package com.j6512.stayfocused;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayfocusedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayfocusedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j6512/stayfocused/StayfocusedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j6512.stayfocused;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayfocusedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
