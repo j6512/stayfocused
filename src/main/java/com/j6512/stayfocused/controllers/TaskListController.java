6b4cac101ce1375433d91d7d990cb4eb19fbeb58
j6512 <justins8287@gmail.com>
2021-04-27 00:36:07 -0500

    Added Edit feature to change a TaskList's name

--- src/main/java/com/j6512/stayfocused/controllers/TaskListController.java
+++ src/main/java/com/j6512/stayfocused/controllers/TaskListController.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -92,5 +90,35 @@ public class TaskListController {
         taskListRepository.save(newTaskList);
 
         return "redirect:/taskList/index";
+    }
+
+    @GetMapping("taskList/edit/{taskListId}")
+    public String displayTaskListEditForm(@PathVariable int taskListId, Model model) {
+        Optional<TaskList> optionalTaskList = taskListRepository.findById(taskListId);
+        TaskList taskList = (TaskList) optionalTaskList.get();
+
+        model.addAttribute("title", "Edit TaskList: " + taskList.getName());
+        model.addAttribute("taskList", taskList);
+        return "taskList/edit";
+    }
+
+    @PostMapping("taskList/edit/{taskListId}")
+    public String processTaskListEditForm(@PathVariable int taskListId, @RequestParam String name, @ModelAttribute @Valid TaskList taskList, Errors errors,
+                                          Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Edit TaskList");
+
+            return "taskList/edit";
+        }
+
+        Optional<TaskList> optionalTaskList = taskListRepository.findById(taskListId);
+        taskList = (TaskList) optionalTaskList.get();
+
+        taskList.setName(name);
+
+        taskListRepository.save(taskList);
+
+        return "redirect:/taskList/index";
+
     }
 }
