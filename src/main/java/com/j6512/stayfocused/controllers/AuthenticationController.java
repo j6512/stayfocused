98b60f5139212a4fa3f861d07402d61cab603240
j6512 <justins8287@gmail.com>
2021-05-01 22:42:08 -0500

    added login greeting, and home page welcomes user

--- src/main/java/com/j6512/stayfocused/controllers/AuthenticationController.java
+++ src/main/java/com/j6512/stayfocused/controllers/AuthenticationController.java
@@ -126,7 +126,7 @@ public class AuthenticationController {
 
         setUserInSession(request.getSession(), theUser);
 
-        model.addAttribute("greeting", "Welcome, " + loginFormDTO.getUsername());
+        model.addAttribute("greeting", "You have successfully logged in, " + loginFormDTO.getUsername());
         return "index";
     }
 
--- src/main/java/com/j6512/stayfocused/controllers/HomeController.java
+++ src/main/java/com/j6512/stayfocused/controllers/HomeController.java
@@ -1,16 +1,45 @@
 package com.j6512.stayfocused.controllers;
 
+import com.j6512.stayfocused.models.User;
+import com.j6512.stayfocused.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
 @Controller
 public class HomeController {
 
-    @RequestMapping("")
-    public String index(Model model) {
+    @Autowired
+    UserRepository userRepository;
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute("user");
+
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> result = userRepository.findById(userId);
+
+        if (result.isEmpty()) {
+            return null;
+        }
+
+        return result.get();
+    }
+    @GetMapping("index")
+    public String index(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        User user = getUserFromSession(session);
+
         model.addAttribute("title", "stay focused");
+        model.addAttribute("greeting", "Hello, " + user.getUserProfile().getFirstName());
 
         return "index";
     }
